595b62d2bb3439998f2a5d9a7f9f3b7a120bf4da
Dabelu123 <dabeluchukwu.nwabueze@myhealthintegral.com>
2026-07-09 10:31:12 +0100

    fixed 2.0

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,24 +1,4 @@
-// import Sidebar from "../components/sidebar/sidebar"";
 
-// import { Outlet } from "react-router-dom";
-
-// const DashboardLayout = () => {
-//   return (
-//     <div className="flex">
-
-//       {/* Sidebar */}
-//       <Sidebar />
-
-//       {/* Main Content */}
-//       <div className="flex-1 p-6 bg-gray-100 min-h-screen">
-//         <Outlet />
-//       </div>
-
-//     </div>
-//   );
-// };
-
-// export default DashboardLayout;
 
 import Sidebar from "../components/sidebar/sidebar";
 import { Outlet } from "react-router-dom";

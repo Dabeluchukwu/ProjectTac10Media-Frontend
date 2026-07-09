5c4f91346892a593c90499ab01f19caf1cff9507
Dabelu123 <dabeluchukwu.nwabueze@myhealthintegral.com>
2026-07-09 10:08:51 +0100

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    // ✅ Remove terser options if you don't need them
+    chunkSizeWarningLimit: 1000,
+  },
 })
